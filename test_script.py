fe8d63802a29c132036029b96112a9a59e398d1d
Stefano Lemerle <stefano.lemerle@gmail.com>
2026-06-01 20:32:46 +0200

    fix: tolto file di debug

--- test_script.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-opts = Options()
-opts.add_argument("--headless=new")
-driver = webdriver.Chrome(options=opts)
-print("Driver started")
-driver.quit()
