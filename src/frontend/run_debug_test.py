a6317fe1bfaf01ef43e5d34bfd01ecff87e2c6c7
Stefano Lemerle <stefano.lemerle@gmail.com>
2026-06-01 20:32:46 +0200

    fix: tolto file di debug

--- src/frontend/run_debug_test.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-pytest.main(["-c", "tests/selenium/pytest.ini", "tests/selenium/tests/test_UC-07_FollowReport.py::TestUC07FollowReport::test_follow_button_present_on_public_report_for_citizen"])
